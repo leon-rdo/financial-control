e31eed6183d8ba9429be762cbd718bf77c3d6fe5
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-07-26 21:34:56 -0300

    feat: add redirect from index to financial_record_list

--- financial/urls.py
+++ financial/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
+from django.views.generic import RedirectView
+
 from .views import *
 
 app_name = "financial"
+
 urlpatterns = [
+    path(
+        "",
+        RedirectView.as_view(pattern_name="financial:financial_record_list", permanent=True),
+        name="index",
+    ),
     path("registros/", FinancialRecordListView.as_view(), name="financial_record_list"),
     path("registros/criar/", FinancialRecordCreateView.as_view(), name="financial_record_create"),
     path("registros/<int:pk>/", FinancialRecordDetailView.as_view(), name="financial_record_detail"),
     path("registros/<int:pk>/editar/", FinancialRecordUpdateView.as_view(), name="financial_record_update"),
     path("parcelas/", InstallmentListView.as_view(), name="installment_list"),
-    path("categorias/", CategoryListView.as_view(), name="category_list")
+    path("categorias/", CategoryListView.as_view(), name="category_list"),
 ]
