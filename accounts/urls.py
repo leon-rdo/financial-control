10b8f4f2a3df513e96e2df17436471da4ece95b7
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-12-08 23:57:44 -0300

    chore: remove old code

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,14 +3,4 @@ from .views import *
 
 
 app_name = "accounts"
-urlpatterns = [
-#     path("entrar/", LoginView.as_view(), name="login"),
-#     path("sair/", LogoutView.as_view(), name="logout"),
-#     path("usuarios/", UserListView.as_view(), name="user_list"),
-#     path("entidades/", EntityListView.as_view(), name="entity_list"),
-#     path("entidades/criar-modal/", EntityModalCreateView.as_view(), name="entity_create_modal"),
-#     path("metodos-de-pagamento/", PaymentMethodListView.as_view(), name="payment_method_list"),
-#     path("metodos-de-pagamento/criar/", PaymentMethodCreateView.as_view(), name="payment_method_create"),
-#     path("metodos-de-pagamento/criar-modal/", PaymentMethodModalCreateView.as_view(), name="payment_method_create_modal"),
-#     path("metodos-de-pagamento/<int:pk>/editar/", PaymentMethodUpdateView.as_view(), name="payment_method_update"),
-]
+urlpatterns = []
