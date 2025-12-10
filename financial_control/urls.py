91259ed526bed957bc87745b604b5d6107030a65
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-12-10 16:02:06 -0300

    chore: remove unused import at financial_control/urls.py

--- financial_control/urls.py
+++ financial_control/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("select2/", include("django_select2.urls")),
     path("", include("core.urls")),
-    path('', include('accounts.urls')),
-    path('', include('financial.urls')),
+    path("", include("accounts.urls")),
+    path("", include("financial.urls")),
 ]
